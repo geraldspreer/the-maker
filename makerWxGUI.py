78954483eaa573e0f52e9a446848323a748b330b
Gerald Spreer - the maker <maker@Spreer-MacBook-Pro.local>
2013-04-18 22:01:39 -0500

    better looking ff2 editor tabs

--- makerWxGUI.py
+++ makerWxGUI.py
@@ -2403,9 +2403,10 @@ class wxPythonGUI(wx.Frame):
         # makerProjectController.py method noteBookPageClosed has to be
         # changed where the noteBoolPages dict is updated
         #
-        self.noteBook = nb.FlatNotebook(self.splitter, wx.ID_ANY, 
+        self.noteBook = nb.FlatNotebook(self.splitter, wx.ID_ANY, agwStyle = wx.lib.flatnotebook.FNB_FF2,
                                         style= wx.lib.flatnotebook.FNB_NODRAG | 
                                         wx.lib.flatnotebook.FNB_X_ON_TAB)
+        self.noteBook.SetPadding(wx.Size(20))
         
         # add a welcome message to the noteBook        
         
